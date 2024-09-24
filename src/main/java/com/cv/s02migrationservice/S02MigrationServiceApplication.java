1f57ed0d196c64454c214d9d0488db8d2163a630
ramakrishna <ram_r@live.com>
2024-09-24 10:50:09 +0400

    Initial commit - migration services - 24-09-2024

---
+++ src/main/java/com/cv/s02migrationservice/S02MigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cv.s02migrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S02MigrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S02MigrationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cv/s02migrationservice/S02MigrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.s02migrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S02MigrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
